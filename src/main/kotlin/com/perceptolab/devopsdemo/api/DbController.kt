760e07c9d1b8e288f2840e9396392fbaba7fa216
Simone Renzo <s.renzo@perceptolab.com>
2022-03-30 15:29:52 +0200

    Implement user delete idempotency

--- src/main/kotlin/com/perceptolab/devopsdemo/api/DbController.kt
+++ src/main/kotlin/com/perceptolab/devopsdemo/api/DbController.kt
@@ -4,6 +4,7 @@ import com.perceptolab.devopsdemo.model.api.AddUserRequest
 import com.perceptolab.devopsdemo.model.db.User
 import com.perceptolab.devopsdemo.repository.UserRepository
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import javax.inject.Inject
@@ -24,5 +25,10 @@ class DbController(@Inject private val userRepository: UserRepository) {
         userRepository.save(User.fromRequest(addUserRequest))
 
     @DeleteMapping("/users/{id}")
-    fun deleteUser(@PathVariable id: Long) = userRepository.deleteById(id)
+    fun deleteUser(@PathVariable id: Long): ResponseEntity<Unit> {
+        if (userRepository.findById(id).isPresent) {
+            userRepository.deleteById(id)
+        }
+        return ResponseEntity.ok().build()
+    }
 }
