07443d5e1c142a6031a92479c359733ab2251ad4
Simone Renzo <s.renzo@perceptolab.com>
2022-03-30 15:24:45 +0200

    Fix user not found by id case

--- src/main/kotlin/com/perceptolab/devopsdemo/api/DbController.kt
+++ src/main/kotlin/com/perceptolab/devopsdemo/api/DbController.kt
@@ -3,13 +3,9 @@ package com.perceptolab.devopsdemo.api
 import com.perceptolab.devopsdemo.model.api.AddUserRequest
 import com.perceptolab.devopsdemo.model.db.User
 import com.perceptolab.devopsdemo.repository.UserRepository
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 import javax.inject.Inject
 
 @RestController
@@ -20,7 +16,8 @@ class DbController(@Inject private val userRepository: UserRepository) {
     fun getUsers(): List<User> = userRepository.findAll()
 
     @GetMapping("/users/{id}")
-    fun getUserById(@PathVariable id: Long): User = userRepository.getById(id)
+    fun getUserById(@PathVariable id: Long): User = userRepository.findById(id)
+        .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND, "User $id not found") }
 
     @PutMapping("/users")
     fun addUser(@RequestBody addUserRequest: AddUserRequest): User =
